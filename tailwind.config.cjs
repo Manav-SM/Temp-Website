6f75c966f470344ea5bf4daf4416759cdd2054be
M Desktop <18483618+Manav-SM@users.noreply.github.com>
2022-05-20 16:38:57 +0530

    added daisy ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			'dark' // default theme for all devices
+		],
+		log: false
+	}
+};
